00781900b45ab9f978dee53d0caa24480164f7e7
Valmir <vladgaspard@yahoo.com>
2022-08-18 13:23:08 -0400

    updated the way the game works To do : Display the letter guessed in the correct index .

--- Web_Scrape.py
+++ Web_Scrape.py
@@ -41,8 +41,9 @@ class Web_Scrape:
               '\nyou Get 7 Tries \n'
               '\n1 hint per word \n'
               '\nPress 1:Guess the whole word\n '
-              '\nPress 2:To skip word \n'
-              '\nPress 3 to end game\n'
+              '\nPress 2:Guess by Letter \n'
+              '\nPress 3:To skip word \n'
+              '\nPress 4 to end game\n'
               '\nnow let us Begin!!\n')
         tries = 0
         self.playGame(self, words, hint, tries)
@@ -103,12 +104,17 @@ class Web_Scrape:
                     print("Thank you for playing")
                     break
             elif userAnswer =='2':
-                self.playGame(self,words,hint)
-            elif userAnswer == '3':
+                self.guessByLetter(self,answer,tries)
+            elif userAnswer =='3':
+                self.playGame(self,words,hint, 0)
+            elif userAnswer == '4':
+                print("Thank you for playing")
                 break
             else:
                 self.checkUserLetter()
 
+
+    #this method validates user answer
     def guessWord(self,userAnswer, answer,tries):
         #this method validates the user answer
         # it starts by check if it is correct and if its not correct
@@ -130,7 +136,7 @@ class Web_Scrape:
                 tries += 1
                 print("Better luck next time\n "
                       "Try again ? (Y/N)")
-                replay= input(">").casefold()
+                replay= input(">").lower()
 
                 while tries <= 8:
                     if replay == "y":
@@ -149,11 +155,92 @@ class Web_Scrape:
 
 
 
-
+    #generates a word to guess
     def generateWord(self,words, hint, randomNum):
-        print("Hint:" + hint[randomNum] + " \nAnswer: " + words[randomNum] + "\n")
+        print("Hint:" + hint[randomNum] + "\n Answer: " + words[randomNum] + "\n" )
+        # nAnswer: " + words[randomNum] + "\n")
         userAnswer = input('>')
         return userAnswer
+
+
+    def guessByLetter(self, answer,tries):
+        # this method user picks a letter
+        #  validate letter wheather its a vowel or consonant
+        # tells the user if its right and the number of tries.
+        #if guessed corectly user wins hangman ,
+        # if not man is hanged
+        #this needs to be in a while loop.
+
+
+        print("Guess by letter , you have {} triess".format(tries))
+        correctGuess= []
+        displayWord=""
+        while tries < 9:
+            letterGuessed = input(">").lower()
+            #check agains words.
+            correctLetters = list(answer)
+            #boolean to check if letter is in list
+            foundLetter= False
+           #add the correct letter to a list to display the correct letters in order.
+            for i in range(len(answer)):
+                if correctLetters[i].lower() == letterGuessed and tries <9:
+                    print("Tureeeeee")
+                    foundLetter = True
+                    break
+                else:
+                    foundLetter= False
+
+#Validates the found letter , add it to
+            if foundLetter:
+                print("Correct !! : {}".format(correctLetters[i]))
+                correctGuess.append(letterGuessed)
+            else:
+                tries += 1
+                print("No Dice \n The man lost {} body parts".format(tries))
+
+
+
+            # display the letters guessed in the correct place
+            # print correct guess in the correct index. in the index where the worst is not correct add a space
+
+            #populate the end of the list to make it equal to the actual answer
+            # check the index and if it not === add a " "
+            while (len(correctLetters)) > (len(correctGuess)):
+                correctGuess.append(" ")
+
+
+
+            count = 0
+            letters =""
+            displayCorrectLetterGuessed = ""
+
+            for words in correctGuess:
+                letters += words
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
